3430277c6568082fd17f4c14b469664da39f9c90
hicham-oualla <hich2022am.oualla@gmail.com>
2024-08-10 23:51:40 +0100

    component  equipment

---
+++ IT_Support_Angulare/src/app/equipment-liste/equipment-liste.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-equipment-liste',
+  templateUrl: './equipment-liste.component.html',
+  styleUrls: ['./equipment-liste.component.css']
+})
+export class EquipmentListeComponent {
+
+}
