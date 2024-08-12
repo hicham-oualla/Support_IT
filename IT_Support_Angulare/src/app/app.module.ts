b96d971a264a224d85a9a50ed789cc6908a97bbb
hicham-oualla <hich2022am.oualla@gmail.com>
2024-08-12 14:24:03 +0100

    pann services

--- IT_Support_Angulare/src/app/app.module.ts
+++ IT_Support_Angulare/src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HttpClientModule} from "@angular/common/http";
+import { EquipementComponent } from './components/equipement/equipement.component';
+import { AddEquipementComponent } from './components/add-equipement/add-equipement.component';
+import {FormsModule} from "@angular/forms";
+import { UpdateEquipementComponent } from './components/update-equipement/update-equipement.component';
+import { CreatePanneComponent } from './components/create-panne/create-panne.component';
+import { PannesComponent } from './components/pannes/pannes.component';
+import { ChangerStatutComponent } from './components/changer-statut/changer-statut.component';
+import { UpdatePanneComponent } from './components/update-panne/update-panne.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EquipementComponent,
+    AddEquipementComponent,
+    UpdateEquipementComponent,
+    CreatePanneComponent,
+    PannesComponent,
+    ChangerStatutComponent,
+    UpdatePanneComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
