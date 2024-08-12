c2b49762c19d73c626b347c0ec2f03ee7a39adb4
hicham-oualla <hich2022am.oualla@gmail.com>
2024-08-12 14:21:06 +0100

    path destintation

--- IT_Support_Angulare/src/app/app-routing.module.ts
+++ IT_Support_Angulare/src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {EquipementComponent} from "./components/equipement/equipement.component";
+import {AddEquipementComponent} from "./components/add-equipement/add-equipement.component";
+import {AppComponent} from "./app.component";
+import {UpdateEquipementComponent} from "./components/update-equipement/update-equipement.component";
+import {CreatePanneComponent} from "./components/create-panne/create-panne.component";
+import {PannesComponent} from "./components/pannes/pannes.component";
+import {ChangerStatutComponent} from "./components/changer-statut/changer-statut.component";
+import {UpdatePanneComponent} from "./components/update-panne/update-panne.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: AppComponent},
+  {path:'addEquipement', component:AddEquipementComponent},
+  {path:'equipements', component: EquipementComponent},
+  {path:'updateEquipement/:id', component: UpdateEquipementComponent},
+  {path:'addPanne', component: CreatePanneComponent},
+  {path:'pannes', component: PannesComponent},
+  {path:'statut/:id', component:ChangerStatutComponent},
+  {path:'updatePanne/:id', component: UpdatePanneComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
